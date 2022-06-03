dad9fb8bf08cd0be0e6ad8c22e19d426557d306b
kurema <kurema_makoto_software@yahoo.co.jp>
2022-06-03 11:33:24 +0900

    Now you can copy and select

--- src/BDVideoLibraryManagerXF/BDVideoLibraryManagerXF/Views/VideoDetailPage.xaml.cs
+++ src/BDVideoLibraryManagerXF/BDVideoLibraryManagerXF/Views/VideoDetailPage.xaml.cs
@@ -41,9 +41,73 @@ namespace BDVideoLibraryManagerXF.Views
             else if (label.FontSize == sizeMedium) label.FontSize = sizeSmall;
         }
 
-        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
         {
+            if (this.BindingContext is not VideoLibraryManagerCommon.Library.DiskVideoPair dvp) return;
+            List<(string text, string key)> options = new();
+            void addText(string text, string key = null)
+            {
+                if (string.IsNullOrWhiteSpace(text)) return;
+                if (key is null) key = text;
+                if (options.Any(a => a.text == text)) return;
+                options.Add((text, key));
+            }
+            addText(dvp.Video?.ProgramTitle);
+            if (dvp.Video?.ProgramTitle is not null)
+            {
+                var temp = dvp.Video?.ProgramTitle;
+                temp = System.Text.RegularExpressions.Regex.Replace(temp, @"\[[^\]]+\]", "");
+                temp = System.Text.RegularExpressions.Regex.Replace(temp, @"^\s+", "");
+                temp = System.Text.RegularExpressions.Regex.Replace(temp, @"\s+$", "");
+                addText(temp);
+            }
+            addText(dvp.Video.ProgramGenre);
+            addText(dvp.Video.ChannelName);
+            addText(dvp.Disk.DiskTitle);
+            addText(dvp.Disk.DiskName);
+            foreach (var item in dvp.Video.Links) addText(item.TextFull);
+            addText(dvp.Video.ProgramDetail, "[動画情報] 全文");
+            const string detailSelect = "[動画情報] 選択";
+            addText(detailSelect, detailSelect);
+            const string cancel = "キャンセル";
 
+            var action = await DisplayActionSheet("コピー", cancel, null, options.Select(a => a.key).ToArray());
+            if (action is null || action == cancel) return;
+            if (action == detailSelect)
+            {
+                await Navigation.PushAsync(new Views.VideoDetailSelectionPage() { BindingContext = dvp.Video });
+                return;
+            }
+            var result = options.FirstOrDefault(a => a.key == action);
+            if (result.text is null) return;
+            await Xamarin.Essentials.Clipboard.SetTextAsync(result.text);
+
+            notifyGrid.IsVisible = true;
+            notifyLabel.Text = "コピーしました。";
+            CopiedText = result.text;
+            await Task.Delay(3000);
+            notifyGrid.IsVisible = false;
+            //~~Toastで通知したいけど、DependencyServiceを使うと今も後もめんどくさい。~~
+        }
+
+        private string CopiedText=null;
+
+        private async void ImageButton_Clicked_Search(object sender, EventArgs e)
+        {
+            try
+            {
+                await Xamarin.Essentials.Browser.OpenAsync($"https://www.google.com/search?q={System.Web.HttpUtility.UrlEncode(CopiedText)}");
+            }
+            catch { }
+        }
+
+        private async void ImageButton_Clicked_Share(object sender, EventArgs e)
+        {
+            try
+            {
+                await Xamarin.Essentials.Share.RequestAsync(CopiedText,"録画情報");
+            }
+            catch { }
         }
     }
 }
---
+++ src/BDVideoLibraryManagerXF/BDVideoLibraryManagerXF/Views/VideoDetailSelectionPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BDVideoLibraryManagerXF.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VideoDetailSelectionPage : ContentPage
+    {
+        public VideoDetailSelectionPage()
+        {
+            InitializeComponent();
+        }
+
+        //選択したテキストに対して色んな操作をしたいけど、カスタムレンダラーなしでは無理なので諦め。
+        //カスタムレンダラー使えば簡単だけどね。
+
+        //private string GetSelected()
+        //{
+        //    try
+        //    {
+        //        return input.Text.Substring(input.CursorPosition, input.SelectionLength);
+        //    }
+        //    catch
+        //    {
+        //        return null;
+        //    }
+        //}
+
+        //private async Task DoActionToText(Func<string, Task> func)
+        //{
+        //    var text = GetSelected();
+        //    if (string.IsNullOrEmpty(text)) return;
+        //    if (func is null) return;
+        //    try
+        //    {
+        //        await func?.Invoke(text);
+        //    }
+        //    catch { }
+        //}
+
+        //private async void ImageButton_Clicked_Search(object sender, EventArgs e) => await DoActionToText(async text => await Xamarin.Essentials.Browser.OpenAsync($"https://www.google.com/search?q={System.Web.HttpUtility.UrlEncode(text)}"));
+
+        //private async void ImageButton_Clicked_Share(object sender, EventArgs e) => await DoActionToText(async text => await Xamarin.Essentials.Share.RequestAsync(text));
+
+        //private async void ImageButton_Clicked_Copy(object sender, EventArgs e)
+        //{
+        //    await DoActionToText(async text => await Xamarin.Essentials.Clipboard.SetTextAsync(text));
+        //}
+    }
+}
